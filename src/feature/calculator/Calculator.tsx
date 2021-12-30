7b744900422e71aa0cf39da24b9499c917b73939
ywj3493 <37992140+ywj3493@users.noreply.github.com>
2021-12-30 10:25:47 +0900

    feat: chart feature update (#8)
    * 20211223 차트1차
    
    * 20211223 차트 1차
    
    * 20211227 chart coloring 1차
    
    * 20211229 chart background 1st
    
    Co-authored-by: ywj3493 <ywj3493@nate.com>

--- src/feature/calculator/Calculator.tsx
+++ src/feature/calculator/Calculator.tsx
@@ -2,14 +2,13 @@ import { Box, Divider, TextField } from "@mui/material";
 import { lazy, Suspense, useState } from "react";
 import TabDescription from "component/TabDescription";
 import ChartSuspense from "./ChartSuspense";
+import { handleNumberStateChange } from "feature/common/reactUtil";
 const ChartBox = lazy(() => import("./Chart"));
 
 function Calculator() {
     const [numPlayer, setNumPlayer] = useState(3);
     const [maxCall, setMaxCall] = useState(3);
     const [numEnd, setNumEnd] = useState(31);
-
-    const parseEvent = (event : React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement> ) => parseInt(event.target.value);
 
     const width = 600;
     const height = 600;
@@ -19,9 +18,9 @@ function Calculator() {
             숫자에 따른 이론적 승률을 계산해서 보여줍니다.
         </TabDescription> 
         <Box sx={{paddingBottom: "2.5rem"}}>
-            <TextField required id="num-player" label="인원수" type="number" value={numPlayer} onChange={(event) => setNumPlayer(parseEvent(event))}/>
-            <TextField required id="max-call" label="최대 말하는 갯수" type="number" value={maxCall} onChange={(event) => setMaxCall(parseEvent(event))}/>
-            <TextField required id="num-end" label="마지막 숫자" type="number" value={numEnd} onChange={(event) => setNumEnd(parseEvent(event))}/> 
+            <TextField required id="num-player" label="인원수" type="number" value={numPlayer} onChange={(event) => handleNumberStateChange(event, setNumPlayer)}/>
+            <TextField required id="max-call" label="최대 말하는 갯수" type="number" value={maxCall} onChange={(event) => handleNumberStateChange(event, setMaxCall, { minVal : 1, maxVal : numEnd })}/>
+            <TextField required id="num-end" label="마지막 숫자" type="number" value={numEnd} onChange={(event) => handleNumberStateChange(event, setNumEnd)}/> 
         </Box>
         <Divider variant="middle" />
         <Suspense fallback={<ChartSuspense width={width} height={height}/>}>
--- src/feature/calculator/Chart.tsx
+++ src/feature/calculator/Chart.tsx
@@ -2,35 +2,103 @@ import { Box } from "@mui/material";
 import { getFullLoseProbMat } from "baskin-lib/dist/lib/strategy";
 import { scaleLinear } from "d3-scale";
 
-// function AxisTick(props) {
-//     const {pos} = props.tick;
-//     return <g className="tick" opacity="1" transform={`translate(${pos}, 0})`}>
-//         <line stroke="currentColor"></line>
-//     </g>
-// }
-
-function ChartPath(props : {
+function AxisTick(props : {
+    width : number,
+    height : number,
     points : {
-        x: number,
         y: number
     }[]
 }) {
-    const {points} = props;
-    return <path fill="none" stroke="blue" strokeWidth="1.5" d={
-        `M ${points[0].x} ${points[0].y}` +
-        points.map(({x, y}) => `L ${x}, ${y}`).join(' ')
-    }/>
+    const { points, width, height } = props;
+    return <g id="axis">
+        <line x="50"></line>
+        {[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1].map((value, idx)=>(
+            <g>
+                <text x="0" y={height*(1-value)}>{`${value*100}%`}</text>
+                <line key={`axisLine${idx}`} stroke="gray" strokeDasharray="5,5" x1={50} y1={height*value} x2={width+100} y2={height*value}/>
+            </g>
+        ))}
+    </g>
 }
 
-function ChartMarkers(props : {
+function ChartFlag(props : {
+    x: number,
+    y: number
+}) {
+    return <></>
+}
+
+function coloringChartMarkers(
+    points : {
+        value : number
+    }[]
+) : string[] {
+    let colors : string[] = ["black", ];
+    let diff : number[] = [];
+    for(let i : number = 1; i < points.length; i++) {
+        diff[i-1] = points[i].value - points[i-1].value;
+    }
+    let flag : string = "";
+    let curr = 1;
+    while(curr<diff.length) {
+        if(diff[curr]>0) {
+            let next = curr+1;
+            while(next<diff.length) {
+                if(diff[next]<0) {
+                    flag = "blue"
+                    break;
+                }else if(diff[next]>0) {
+                    flag = "black"
+                    break;
+                }else {
+                    next++;   
+                }
+            }
+            for(let j : number = curr; j < next; j++) {
+                colors[j+1] = flag;
+            }
+            curr += next-curr;
+        }else if(diff[curr]<0) {
+            let next = curr+1;
+            while(next<diff.length) {
+                if(diff[next]<0) {
+                    flag = "black"
+                    break;
+                }else if(diff[next]>0) {
+                    flag = "red"
+                    break;
+                }else {
+                    next++;
+                }
+            }
+            for(let j : number = curr; j < next; j++) {
+                colors[j+1] = flag;
+            }
+            curr += next-curr;
+        }else {
+            colors[curr+1] = flag;
+            curr++;
+        }
+    }
+    colors[points.length - 1] = "black";
+    return colors; 
+}
+
+function ChartGraph(props : {
     points : {
         x : number,
-        y : number
+        y : number,
+        value : number
     }[]
 }) {
-    const {points} = props;
+    const { points } = props;
+    const colors = coloringChartMarkers(points);
     return <g>
-        {points.map(({x, y}, idx) => <circle key={`${idx}`} cx={x} cy={y} r="5" fill="black" tabIndex={idx}/>)}
+        <path fill="none" stroke="black" strokeWidth="1.5" d={
+            `M ${points[0].x+50} ${points[0].y}` +
+            points.map(({x, y}) => `L ${x+50}, ${y}`).join(' ')
+        }/>
+        {points.map(({x, y, value}, idx) => <circle key={`${idx}`} cx={x+50} cy={y} r="5" fill={colors[idx]} tabIndex={idx} onFocus={()=>{console.dir(`x, y, value = ${x}, ${y}, ${value}`)}}/>)}
     </g>
 }
 
@@ -47,17 +115,12 @@ function ChartBox(props : {
 
     const xScale = scaleLinear().domain([0, data.length-1]).range([0, width]);
     const yScale = scaleLinear().domain([0, 1]).range([0, height]);
-    const points = data.map((p, idx) => ({x: xScale(idx), y: yScale(p)}))
+    const points = data.map((p, idx) => ({x: xScale(idx), y: yScale(p), value: p}));
 
     return <Box sx={{p: 3, width: width, height: height}}>
-        <svg width={width} height={height}>
-            <g>
-
-            </g>
-            <g>
-                <ChartPath points={points} />
-                <ChartMarkers points={points}/>
-            </g>
+        <svg viewBox={`-20 -20 ${width+100} ${height+100}`} width={width} height={height}>
+            <AxisTick points={points} width={width} height={height} />
+            <ChartGraph points={points}/>
         </svg>
     </Box>
 }
